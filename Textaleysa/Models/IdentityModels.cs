117f75e6233c1dea0c8105c5a6c75ab0c21d40bc
arnarkari93 <arnarkari93@gmail.com>
2014-05-08 18:03:20 +0000

    HRConnetion

--- Textaleysa/Models/IdentityModels.cs
+++ Textaleysa/Models/IdentityModels.cs
@@ -10,7 +10,7 @@ namespace Textaleysa.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("HRConnection")
         {
         }
     }
