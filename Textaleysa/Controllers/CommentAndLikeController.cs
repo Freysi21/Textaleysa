5135eae9baae51b2a8d1a6e4be867db44d78ba65
arnarkari93 <arnarkari93@gmail.com>
2014-05-08 20:51:12 +0000

    Added CommentAndLikeController

---
+++ Textaleysa/Controllers/CommentAndLikeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Textaleysa.Models;
+using Textaleysa.Models.Repositories;
+
+namespace Textaleysa.Controllers
+{
+    public class CommentAndLikeController : Controller
+    {
+        //
+        // GET: /CommentAndLike/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		// Gets all the comments
+		public ActionResult GetComments()
+		{
+			return View();
+		}
+
+		// Posts a new comment
+		public ActionResult PostComment(Comment comment)
+		{
+			return View();
+		}
+
+
+		public ActionResult GetLikes()
+		{
+			return View();
+		}
+
+		public ActionResult PostLike(Like like)
+		{
+			return View();
+		}
+	}
+}
