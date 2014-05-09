81d937098cbb865295c0bd077c993e0f31d82f69
arnarkari93 <arnarkari93@gmail.com>
2014-05-09 14:27:25 +0000

    Added SubtitleFileController.cs

---
+++ Textaleysa/Controllers/SubtitleFileController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Textaleysa.Controllers
+{
+    public class SubtitleFileController : Controller
+    {
+        //
+        // GET: /SubtitleFile/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
