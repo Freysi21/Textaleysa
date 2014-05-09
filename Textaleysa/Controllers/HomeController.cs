e1095aab11ebf5adc895008776790ed309046112
arnarkari93 <arnarkari93@gmail.com>
2014-05-09 13:46:58 +0000

    Reset the ActionResult About

--- Textaleysa/Controllers/HomeController.cs
+++ Textaleysa/Controllers/HomeController.cs
@@ -17,36 +17,10 @@ namespace Textaleysa.Controllers
 			return View();
 		}
 
-        [HttpPost]
-        public ActionResult About(string title, int year, int season, int episode, HttpPostedFileBase file, ApplicationUser user)
-        {
-            
-            if (file.ContentLength > 0)
-            {
-                if (year == 0)
-                {
-                    Serie serie = new Serie();
-                    serie.season = season;
-                    serie.episode = episode;
-                    serie.title = title;
-                    MediaTitleRepository.Instance.AddMediaTitle(serie);
-                }
-                else
-                {
-                    
-                }
-                string newFile = file.ToString();
-                SubtitleFile subs = new SubtitleFile();
-                subs.content = newFile;
-                subs.userID = Int32.Parse(user.Id);
-                subs.date = DateTime.Now;
-                //vantar language
-                subs.downloadCount = 0;
-                SubtitleFileRepository.Instance.AddFile(subs);
-            }
-
-            return RedirectToAction("Index");
-        }
+		public ActionResult About()
+		{
+			return View();
+		}
 
 		public ActionResult Contact()
 		{
