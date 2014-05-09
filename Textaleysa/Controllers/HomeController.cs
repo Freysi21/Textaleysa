392d374e80f7f7c666386b7b0115da8a59d4b478
gunnark13 <gunnark13@ru.is>
2014-05-09 23:34:02 +0100

    scripta i layout faerd i comment and like kv.addihakkari

--- Textaleysa/Controllers/HomeController.cs
+++ Textaleysa/Controllers/HomeController.cs
@@ -17,40 +17,6 @@ namespace Textaleysa.Controllers
 			return View();
 		}
 
-<<<<<<< HEAD
-		public ActionResult About() 
-		{ 
-			return View();
-		}
-        [HttpPost]
-        public ActionResult About(string title, int year, int season, int episode, HttpPostedFileBase file, ApplicationUser user)
-        {
-            
-            if (file.ContentLength > 0)
-            {
-                if (year == 0)
-                {
-                    Serie serie = new Serie();
-                    serie.season = season;
-                    serie.episode = episode;
-                    serie.title = title;
-                    MediaTitleRepository.Instance.AddMediaTitle(serie);
-                }
-                else
-                {
-                    
-                }
-                string newFile = file.ToString();
-                SubtitleFile subs = new SubtitleFile();
-                subs.content = newFile;
-                subs.userID = Int32.Parse(user.Id);
-                subs.date = DateTime.Now;
-                //vantar language
-                subs.downloadCount = 0;
-                SubtitleFileRepository.Instance.AddFile(subs);
-            }
-            return RedirectToAction("Index");  
-=======
 		public ActionResult Help()
 		{
 			ViewBag.Message = "Tekka hvort help message virkar frá HomeController";
@@ -76,7 +42,7 @@ namespace Textaleysa.Controllers
 			ViewBag.Message = "Tekka hvort about message virkar frá HomeController";
 
 			return View();
->>>>>>> 400658eb6585affe6bb5db68b16473d695393cc3
+
         }
 
 		public ActionResult Contact()
