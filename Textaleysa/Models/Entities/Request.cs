739a3aa31c3fa769a2e0c3563b60077cd71d7458
freysi21 <freysi21@gmail.com>
2014-05-11 18:34:35 +0100

    Ég vill hafa Username frekar en userID, change dat. VoteRepository RequestRepository RequestController

--- Textaleysa/Models/Entities/Request.cs
+++ Textaleysa/Models/Entities/Request.cs
@@ -8,7 +8,7 @@ namespace Textaleysa.Models
 	public class Request
 	{
 		public int ID { get; set; }
-		public int userID { get; set; }
+		public string userName { get; set; }
 		public DateTime date { get; set; }
 		public string mediaType { get; set; }
 		public string mediaTitle { get; set; }
