f9a366ee273ee9592dcfb9f78813c8a652eda459
freysi21 <freysi21@gmail.com>
2014-05-11 17:52:02 +0100

    RequestController

---
+++ Textaleysa/Controllers/RequestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Textaleysa.Models.Repositories;
+
+namespace Textaleysa.Controllers
+{
+    public class RequestController : Controller
+    {
+        //
+        // GET: /Request/
+        public ActionResult Request()
+        {
+            var model = RequestRepository.Instance.GetRequests();
+            return View(model);
+        }
+	}
+}
