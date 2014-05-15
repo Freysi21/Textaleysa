f7c3245bdcc6e7253ae163be01b4e8eba74c2796
freysi21 <freysi21@gmail.com>
2014-05-15 17:21:33 +0100

    doin gradeScript

---
+++ Textaleysa/Scripts/GradeScript.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    alert("hallo");
+    jQuery(".vote-request").click(function () {
+        var new_vote = { requestID: this.id }
+        jQuery.post("/Request/postVotes", new_vote, function (data) {
+            getVote(data);
+        });
+    });
+});
+function getVote(new_vote) {
+    $.ajax({
+        type: "GET",
+        contentType: "application/json; charset=utf­8",
+        url: "/Request/getVotes/",
+        data: new_vote,
+        dataType: "json",
+        success: function (votes) {
+
+            $('#vote-' + votes[0].requestID).html(votes.length);
+        },
+        error: function (xhr, err) {
+            // Note: just for debugging purposes!
+            alert("readyState: " + xhr.readyState +
+            "\nstatus: " + xhr.status);
+            alert("responseText: " + xhr.responseText);
+        }
+    });
+}
