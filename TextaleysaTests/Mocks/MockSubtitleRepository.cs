dda0e5717e5da938f3bb663541a7331dcb4ba896
eysteinn13 <eysteinngu@gmail.com>
2014-05-15 18:26:40 +0000

    fixed nunit

---
+++ TextaleysaTests/Mocks/MockSubtitleRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Textaleysa.Models;
+using Textaleysa.Models.Repositories;
+using Textaleysa.Repositories;
+
+namespace TextaleysaTests.Mocks
+{
+    public class MockSubtitleRepository : ISubtitleRepository
+    {
+        private readonly List<SubtitleFile> _subtitleFiles;
+        public MockSubtitleRepository(List<SubtitleFile> subtitleFiles)
+        {
+            _subtitleFiles = subtitleFiles;
+        }
+        public IQueryable<Textaleysa.Models.SubtitleFile> GetAllSubtitles()
+        {
+            return _subtitleFiles.AsQueryable();
+        }
+    }
+}
