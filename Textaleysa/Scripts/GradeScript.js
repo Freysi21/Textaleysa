c352589f392e6be9bc5365312a8e4c90036b169c
freysi21 <freysi21@gmail.com>
2014-05-15 23:42:59 +0100

    GradeScript

--- Textaleysa/Scripts/GradeScript.js
+++ Textaleysa/Scripts/GradeScript.js
@@ -1,25 +1,22 @@
 ﻿$(document).ready(function () {
-<<<<<<< HEAD
     jQuery(".submit-grade").click(function () {
-        var give_grade = $("input#" + this.id).val();
+        var give_grade = $("#input-" + this.id).val();
         if (give_grade > 10 || give_grade < 0) {
+            $("#error-message").html("Einkunn verður að vera á bilinu 0-10")
             //alert("Einkunn verður að vera á bilinu 0-10");
         }
-        var new_grade = { fileID: this.id, mediaGrade: give_grade  }
-        jQuery.post("/SubtitleFile/postGrade", new_grade, function (data) {
-            if (data == "") {
-                //alert("BÍDDU HALLÓ, ÆTLAR ÞÚ GEFA EINKUNNIR EINS OG KENNARI Í HR Í LOK ANNAR");
-            }
-            else {
-                getVote(data);
-            }
-=======
-    jQuery(".vote-request").click(function () {
-        var new_vote = { requestID: this.id }
-        jQuery.post("/Request/postVotes", new_vote, function (data) {
-            getVote(data);
->>>>>>> 30dcd4a252c8c39737735768fc4e5f8a207fdd79
-        });
+        else{
+            var new_grade = { fileID: this.id, mediaGrade: give_grade }
+
+            jQuery.post("/SubtitleFile/postGrade", new_grade, function (data) {
+                if (data == "") {
+                    $("#error-message").html("Ekki er hægt að gefa sömu skrá einkunn tvisvar.")
+                }
+                else {
+                    getVote(data);
+                }
+            });
+        }
     });
 });
 function getVote(new_grade) {
@@ -41,3 +38,4 @@ function getVote(new_grade) {
         }
     });
 }
+        
