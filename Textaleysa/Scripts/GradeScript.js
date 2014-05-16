dc4c7c619d21763adacd39e1dfa252838f3bfff0
freysi21 <freysi21@gmail.com>
2014-05-16 03:31:50 +0100

    Grade Works

--- Textaleysa/Scripts/GradeScript.js
+++ Textaleysa/Scripts/GradeScript.js
@@ -2,13 +2,7 @@
     jQuery(".submit-grade").click(function () {
         var give_grade = $("#input-" + this.id).val();
         if (give_grade > 10 || give_grade < 0) {
-<<<<<<< HEAD
-            console.log("Einkunn verður að vera á bilinu 0-10");
             $("#error-message").html("Einkunn verður að vera á bilinu 0-10")
-            //alert("Einkunn verður að vera á bilinu 0-10");
-=======
-            $("#error-message").html("Einkunn verður að vera á bilinu 0-10")
->>>>>>> f036f0441b56c7602e9c5ef51a6280f875424a0d
         }
         else{
             var new_grade = { fileID: this.id, mediaGrade: give_grade }
